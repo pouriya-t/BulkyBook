b14ec4e4d6033ee3b895afb5f454b3b598416f93
Pouriya <pouriya.un@gmail.com>
2020-10-14 13:44:56 +0330

    End of section 7

---
+++ BulkyBook.DataAccess/Repository/IRepository/IUnitOfWork.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BulkyBook.DataAccess.Repository.IRepository
+{
+    public interface IUnitOfWork : IDisposable
+    {
+        ICategoryRepository Category { get; }
+
+        ISP_Call SP_Call { get; }
+
+        void Save();
+    }
+}
